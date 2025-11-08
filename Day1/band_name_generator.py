7a806841939fc7f8b86cf42cfffb051f18aa4331
Shmruti55 <patroshmruti2689@gmail.com>
2025-11-08 20:20:00 +0530

    created band_name_generator.py

--- Day1/band_name_generator.py
+++ Day1/band_name_generator.py
@@ -1,3 +1,16 @@
+'''
+\n : => next line(new line escape)
+concatenation and spacing: => print("Hello" + " " + "World")
+print(), input(): => print("Hello " + input(" what is your name?"))
+len: =>print(len(input("What is your name?")))
+variable naming: =>
+glass1 = "milk"
+glass2 = "juice"
+temp = glass1
+glass1 = glass2
+glass2 = temp
+'''
+
 print("Click Run to run the final project you will build")
 print("Welcome to the Band Name Generator.")
 a = input("What's the name of the city you grew up in?\n")
