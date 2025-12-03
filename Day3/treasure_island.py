1963fe1f1bc5e415e1eb60a340a6256e280780b2
Shmruti55 <patroshmruti2689@gmail.com>
2025-12-04 00:25:50 +0530

    Update treasure_island.py

--- Day3/treasure_island.py
+++ Day3/treasure_island.py
@@ -1,3 +1,77 @@
+'''
+if-else statement=>
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry you have to grow taller beore you can ride")
+
+comparison operator=> >,,, >=, <=, ==(check equality),!=
+# = => assigning th value
+modulo operator % => return remainder
+nested if else=> if, elif, else
+multiple if=> every condition is checked. 3 in if-e;se only one condition is  checked and returned
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age?"))
+    if age <= 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+        elif age >= 45 and age <= 55:
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+    wants_photo = input("Do you want to have a photo take ? type y for Yes and n for No")
+    if wants_photo == "y":
+        bill = bill + 3 #bill += 3
+    print(f"Your final bill is $ {bill}")
+else:
+    print("Sorry you have to grow taller beore you can ride")
+
+
+******Python Pizza******
+print("Welcome to Python Pizza Deliveries!")
+bill = 0
+size = input("What size pizza do you want? S, M or L: ")
+if size == "S":
+    bill = 15
+elif size == "M":
+    bill = 20
+elif size == "L":
+    bill = 25
+else:
+    print("Invalid pizza size!")
+    exit()
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+if extra_cheese == "Y":
+    bill += 1
+print(f"Your total bill is: ${bill}")
+
+logical operator=> and, or, not
+
+
+coding exercise 5=>
+if bmi < 18.5:
+    print("underweight")
+elif 18.5 <= bmi < 25:
+    print("normal weight")
+else:
+    print("overweight")
+'''
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
