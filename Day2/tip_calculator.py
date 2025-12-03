a73481905b24115e0b1d3bfddc31f8b37e95792b
Shmruti55 <patroshmruti2689@gmail.com>
2025-12-03 15:45:01 +0530

    Update tip_calculator.py

--- Day2/tip_calculator.py
+++ Day2/tip_calculator.py
@@ -1,10 +1,27 @@
 '''
+string subscripting=> print("Hello"[0])
+primitive data types=>sting, integers, float(decimal), boolean(true. false)
+type function=> print(type("Hello"))
+type conversion/casting=> print(int("123")+ int("456"))
+mathematical operation=> +, -, *, /, //( single slash for floating o/p and // for int o/p, It 
+does not round off the no but remove the digits after decimal point). **(exponential/power)
+priority sequence PEMDAS=> (),**,*,/,+,-
+rounding function=> 
+bmi = 78/ 1.43 ** 2
+print(round(bmi,2))
+assignment operator=> +=. -=, *=, /=
+f-strings=> to insert a variable or an expression into a string.
+
+score =0
+height = 1.8
+is-winning = True
+print(f"Your score is = {score}, your height is {height}. Your are winning is {is_winning}")
 
 
-
-
-
-
+coding exercise 4=> BMI Calculator
+height = 1.65 
+weight = 84
+bmi = 84 / ( 1.65 ** 2 )
 
 '''
 
