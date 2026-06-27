bf7aa801ba48609195ccc87f05421bbb4aa239e2
Shmruti55 <patroshmruti2689@gmail.com>
2026-06-27 11:29:55 +0000

    Day 2 completed

--- Day2/CodingE_4.py
+++ Day2/CodingE_4.py
@@ -2,7 +2,7 @@
 The body mass index (BMI) is a measure used in medicine to see if someone is underweight or overweight. 
 This is the formula used to calculate it:
 bmi is equal to the person's weight divided by the person's height squared.
-Convert this sentence into code on line 6.
+Convert this into sentence into code on line 6.
 '''
 height = 1.65 
 weight = 84
